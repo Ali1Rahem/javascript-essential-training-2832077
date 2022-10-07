1e7de8ed75cc00dbe616eabc46b372c2de0832ac
Ali1Rahem <115199695+Ali1Rahem@users.noreply.github.com>
2022-10-06 23:55:04 -0400

    add new commit

--- 03_10/script.js
+++ 03_10/script.js
@@ -16,3 +16,4 @@ const everydayPack = new Backpack(
 
 console.log("The everydayPack object:", everydayPack);
 console.log("The pocketNum value:", everydayPack.pocketNum);
+console.log("the volume vlaue:", everydayPack.volume);
